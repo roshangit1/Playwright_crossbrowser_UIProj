a7fbdb2189a93a807ddbc791836a71b38ae9f52b
Roshan Nair <roshannair89@gmail.com>
2025-12-22 11:41:05 -0700

    Delete stepzero references

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,11 +5,12 @@ export default defineConfig({
   timeout: 30 * 1000,
   retries: 1,
   use: {
-    baseURL: 'https://playwright.dev/',
-    headless: true,
-    screenshot: 'on',
-    video: 'on',
+    headless: true,                // run in headless mode by default
+    screenshot: 'only-on-failure', // capture screenshots on failure
+    video: 'retain-on-failure',    // record video on failure
+    trace: 'retain-on-failure',    // keep trace files for debugging
   },
+
   projects: [
     { name: 'Chromium', use: { ...devices['Desktop Chrome'] } },
     { name: 'Firefox', use: { ...devices['Desktop Firefox'] } },
